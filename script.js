3abc043ebf8558a7272f31a85cdcd722bfdc42e2
FernandoBlanco10 <blanco.alvarez.moy2002@gmail.com>
2025-07-15 01:23:55 -0600

    Actualización de página y estilos.

--- script.js
+++ script.js
@@ -30,32 +30,11 @@ document.querySelectorAll(".nav-links a").forEach(link => {
 // Efecto parpadeo suave en los iconos al pasar el cursor
 document.querySelectorAll('.skill i').forEach(icon => {
   icon.addEventListener('mouseenter', () => {
-    icon.style.animation = 'glow 1s ease-in-out infinite alternate';
+      icon.style.animation = 'glow 1s ease-in-out infinite alternate';
   });
   icon.addEventListener('mouseleave', () => {
-    icon.style.animation = 'none';
+      icon.style.animation = 'none';
   });
-});
-
-// Animación suave al hacer scroll a las secciones
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    const target = document.querySelector(this.getAttribute('href'));
-    if (target) {
-    target.scrollIntoView({ behavior: 'smooth' });
-    }
-});
-});
-
-// Efecto parpadeo suave en los iconos al pasar el cursor
-document.querySelectorAll('.skill i').forEach(icon => {
-icon.addEventListener('mouseenter', () => {
-    icon.style.animation = 'glow 1s ease-in-out infinite alternate';
-});
-icon.addEventListener('mouseleave', () => {
-    icon.style.animation = 'none';
-});
 });
 
 //**************************************************************
